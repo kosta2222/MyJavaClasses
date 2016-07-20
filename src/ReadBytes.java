f62b9068f36d1dc34c148d4698b5d7f7c88a7b3a
kosta2222 <muh_kosta@mail.ru>
2016-07-20 17:01:34 +0300

    new right

--- src/ReadBytes.java
+++ src/ReadBytes.java
@@ -3,49 +3,60 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.StringTokenizer;
+
 
 class ReadBytes{
     FileRider mr;
     BufferedReader bf;
-    ArrayList l;
+    ArrayList <String> l;
+    StringBuilder sb;		
 
-    public ReadBytes(File f,String charset,char delimeter) {
+    public ReadBytes(File f,String charset) {
          mr=new FileRider(f,charset);
-         l=new ArrayList<String>();
+         //l=new ArrayList<String>();
+	sb=new StringBuilder();
     }
-    public ArrayList<String> read(){
+    public ArrayList<String> read(char delimeter){
    
         int charRead = 0;
 char buffer[]=new char[256];//буфер для строки из цикла
 char bufferOut[];
-int i = 0;
+
+
      bf=mr.getBuffered_reader();
     try{
 if (bf != null){
+int i = 0;
 while (charRead != -1){
 charRead = bf.read();
-if ((char)charRead != ';'){
-buffer[i] = (char)charRead;
-i++;
+
+if ((char)charRead !=delimeter){
+    
+
+String s=String.valueOf((char)charRead);
+sb.append(s);
+
 }
-         
+else if((char)charRead ==delimeter)
+    sb.append(';');
+
+   
         
          
   }
+    System.out.println(sb);
+    StringTokenizer st=new StringTokenizer(sb.toString(),"=",false);
+while(st.hasMoreTokens()){
+String StringStmp=st.nextToken();
 
 }
-
+}
 }catch(IOException ioEx){
 System.err.println("Oshibka chteniya faila["+ioEx+"]");
 
 }
-    bufferOut=new char[i];
-    for(int j=0;j<i-1;j++){
-        bufferOut[j]=buffer[j];
-    }
-    String str=new String(bufferOut);
-    l.add(str);
+        
 try{
  mr.getBuffered_reader().close();
 
@@ -56,4 +67,12 @@ System.err.println("Oshibka zakritiya faila["+ioEx+"]");
 return l;
 }
     }
+
+class Main  {
+public static void main(String args[]){
+File f=new File("D:\\NetBeansProjects\\MyOwnLan\\src\\text.dat");
+    ReadBytes rb=new ReadBytes(f,"cp1251");
+     rb.read(']');
+}
+}
 
